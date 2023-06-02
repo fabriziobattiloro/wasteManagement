dc8fe6fe84cc1a4d63180436d07dce18c4be1ea1
fabriziobattiloro <129081348+fabriziobattiloro@users.noreply.github.com>
2023-06-02 21:20:18 +0200

    v1

---
+++ binary/train_BiSeNet.py
@@ -0,0 +1,128 @@
+import os
+import random
+
+import torch
+from torch import optim
+from torch.autograd import Variable
+from torch.nn import NLLLoss2d
+from torch.optim.lr_scheduler import StepLR
+from torchvision.utils import save_image
+import torchvision.transforms as standard_transforms
+import torchvision.utils as vutils
+from tensorboardX import SummaryWriter
+
+from models.model_BiSeNet2 import BiSeNetV2
+from models.config import cfg, __C
+from models.loading_data import loading_data
+from models.utils import *
+from models.timer import Timer
+import pdb
+
+exp_name = cfg.TRAIN.EXP_NAME
+log_txt = cfg.TRAIN.EXP_LOG_PATH + '/' + exp_name + '.txt'
+writer = SummaryWriter(cfg.TRAIN.EXP_PATH+ '/' + exp_name)
+
+pil_to_tensor = standard_transforms.ToTensor()
+train_loader, val_loader, restore_transform = loading_data()
+
+def main():
+
+    cfg_file = open('/content/drive/MyDrive/project-WasteSemSeg-main_3/binary/models/config.py',"r")  
+    cfg_lines = cfg_file.readlines()
+    
+    with open(log_txt, 'a') as f:
+            f.write(''.join(cfg_lines) + '\n\n\n\n')
+    if len(cfg.TRAIN.GPU_ID)==1:
+        torch.cuda.set_device(cfg.TRAIN.GPU_ID[0])
+    torch.backends.cudnn.benchmark = True
+
+    net = []   
+    net = BiSeNetV2( cfg.DATA.NUM_CLASSES)
+    
+
+    if len(cfg.TRAIN.GPU_ID)>1:
+        net = torch.nn.DataParallel(net, device_ids=cfg.TRAIN.GPU_ID).cuda()
+    else:
+        net=net.cuda()
+
+    net.train()
+    criterion = torch.nn.BCEWithLogitsLoss().cuda() # Binary Classification
+    optimizer = optim.Adam(net.parameters(), lr=cfg.TRAIN.LR, weight_decay=cfg.TRAIN.WEIGHT_DECAY)
+    scheduler = StepLR(optimizer, step_size=cfg.TRAIN.NUM_EPOCH_LR_DECAY, gamma=cfg.TRAIN.LR_DECAY)
+    _t = {'train time' : Timer(),'val time' : Timer()} 
+    validate(val_loader, net, criterion, optimizer, -1, restore_transform)
+    for epoch in range(cfg.TRAIN.MAX_EPOCH):
+        _t['train time'].tic()
+        train(train_loader, net, criterion, optimizer, epoch)
+        _t['train time'].toc(average=False)
+        print('training time of one epoch: {:.2f}s'.format(_t['train time'].diff))
+        _t['val time'].tic()
+        validate(val_loader, net, criterion, optimizer, epoch, restore_transform)
+        _t['val time'].toc(average=False)
+        print('val time of one epoch: {:.2f}s'.format(_t['val time'].diff))
+
+
+def train(train_loader, net, criterion, optimizer, epoch):
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        inputs = Variable(inputs).cuda()
+        labels = Variable(labels).cuda()
+        
+        outputs = net(inputs)
+        out_aux, out_aux2, out_aux3, out_aux4, out_aux5= outputs
+        loss1 = criterion(out_aux, labels.unsqueeze(1).float())
+        loss2 = criterion(out_aux2, labels.unsqueeze(1).float())
+        loss3 = criterion(out_aux3, labels.unsqueeze(1).float())
+        loss4 = criterion(out_aux4, labels.unsqueeze(1).float())
+        loss5 = criterion(out_aux5, labels.unsqueeze(1).float())
+        loss=loss1+loss2+loss3+loss4+loss5
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+
+def validate(val_loader, net, criterion, optimizer, epoch, restore):
+    net.eval()
+    criterion.cpu()
+    input_batches = []
+    output_batches = []
+    label_batches = []
+    iou_ = 0.0
+    for vi, data in enumerate(val_loader, 0):
+        inputs, labels = data
+        inputs = Variable(inputs, volatile=True).cuda()
+        labels = Variable(labels, volatile=True).cuda()
+        outputs = net(inputs)
+        #for binary classification
+        
+        out_aux, out_aux2, out_aux3, out_aux4, out_aux5= outputs
+        out_aux[out_aux > 0.5] = 1
+        out_aux[out_aux <= 0.5] = 0
+        #for multi-classification ???
+
+        iou_ += calculate_mean_iu([out_aux.squeeze_(1).data.cpu().numpy()], [labels.data.cpu().numpy()], 2)
+
+    mean_iu = iou_/len(val_loader) 
+    flops, params = compute_flops(net)
+    model_size = compute_model_size(net)
+
+
+    print('[mean iu %.4f]' % (mean_iu)) 
+    print('Model size %.4f' % (model_size))
+    print("flops:"+flops)
+    print("params:"+params)
+    net.train()
+    criterion.cuda()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
---
+++ multiclass/models/utils.py
@@ -0,0 +1,232 @@
+from torch import nn
+import torch.nn.functional as F
+import numpy as np
+from PIL import Image
+import os
+import shutil
+from .config import cfg
+import torch.cuda.amp as amp
+import torch
+from thop import profile, clever_format
+import torch.distributed as dist
+
+def weights_init_kaiming(m):
+    if isinstance(m, nn.Conv2d):
+        #kaiming is first name of author whose last name is 'He' lol
+        nn.init.kaiming_uniform(m.weight) 
+        m.bias.data.zero_()
+
+def adjust_learning_rate(lr, decay, optimizer, cur_epoch, n_epochs):
+    """Sets the learning rate to the initially 
+        configured `lr` decayed by `decay` every `n_epochs`"""
+    new_lr = lr * (decay ** (cur_epoch // n_epochs))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = new_lr
+
+def calculate_mean_iu(predictions, gts, num_classes):
+    sum_iu = 0
+    mean_classes = []
+    for i in range(num_classes):
+        n_ii = t_i = sum_n_ji = 1e-9
+        for p, gt in zip(predictions, gts):
+            n_ii += np.sum(gt[p == i] == i)
+            t_i += np.sum(gt == i)
+            sum_n_ji += np.sum(p == i)
+        mean_i = float(n_ii) / (t_i + sum_n_ji - n_ii)
+        mean_classes.append(mean_i)
+    #mean_classes.append(sum_iu / num_classes)
+    mean_tot = sum(mean_classes) / num_classes  # Calcola la media delle classi totali correttamente
+    mean_classes.append(mean_tot)
+    
+    return mean_classes[0], mean_classes[1], mean_classes[2], mean_classes[3], mean_classes[4], mean_classes[5]
+#CrossEntropyLoss2d
+class CrossEntropyLoss2d(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(CrossEntropyLoss2d, self).__init__()
+        self.nll_loss = nn.NLLLoss2d(weight, size_average)
+
+    def forward(self, inputs, targets):
+        return self.nll_loss(F.log_softmax(inputs, dim=-1), targets)
+#CustomLoss
+class CustomLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(CustomLoss, self).__init__()
+        self.nll_loss = nn.NLLLoss(weight, size_average)
+
+    def forward(self, inputs, targets):
+        return self.nll_loss(F.log_softmax(inputs, dim=1), targets)
+
+#Focal loss
+
+class FocalSigmoidLossFuncV2(torch.autograd.Function):
+    '''
+    compute backward directly for better numeric stability
+    '''
+    @staticmethod
+    @amp.custom_fwd(cast_inputs=torch.float32)
+    def forward(ctx, logits, label, alpha, gamma):
+        #  logits = logits.float()
+
+        probs = torch.sigmoid(logits)
+        coeff = (label - probs).abs_().pow_(gamma).neg_()
+        log_probs = torch.where(logits >= 0,
+                F.softplus(logits, -1, 50),
+                logits - F.softplus(logits, 1, 50))
+        log_1_probs = torch.where(logits >= 0,
+                -logits + F.softplus(logits, -1, 50),
+                -F.softplus(logits, 1, 50))
+        ce_term1 = log_probs.mul_(label).mul_(alpha)
+        ce_term2 = log_1_probs.mul_(1. - label).mul_(1. - alpha)
+        ce = ce_term1.add_(ce_term2)
+        loss = ce * coeff
+
+        ctx.vars = (coeff, probs, ce, label, gamma, alpha)
+
+        return loss
+
+    @staticmethod
+    @amp.custom_bwd
+    def backward(ctx, grad_output):
+        '''
+        compute gradient of focal loss
+        '''
+        (coeff, probs, ce, label, gamma, alpha) = ctx.vars
+
+        d_coeff = (label - probs).abs_().pow_(gamma - 1.).mul_(gamma)
+        d_coeff.mul_(probs).mul_(1. - probs)
+        d_coeff = torch.where(label < probs, d_coeff.neg(), d_coeff)
+        term1 = d_coeff.mul_(ce)
+
+        d_ce = label * alpha
+        d_ce.sub_(probs.mul_((label * alpha).mul_(2).add_(1).sub_(label).sub_(alpha)))
+        term2 = d_ce.mul(coeff)
+
+        grads = term1.add_(term2)
+        grads.mul_(grad_output)
+
+        return grads, None, None, None
+
+class FocalLossV2(nn.Module):
+
+    def __init__(self,
+                 alpha=0.25,
+                 gamma=2,
+                 reduction='mean'):
+        super(FocalLossV2, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, logits, label):
+        '''
+        Usage is same as nn.BCEWithLogits:
+            >>> criteria = FocalLossV2()
+            >>> logits = torch.randn(8, 19, 384, 384)
+            >>> lbs = torch.randint(0, 2, (8, 19, 384, 384)).float()
+            >>> loss = criteria(logits, lbs)
+        '''
+        loss = FocalSigmoidLossFuncV2.apply(logits, label, self.alpha, self.gamma)
+        if self.reduction == 'mean':
+            loss = loss.mean()
+        if self.reduction == 'sum':
+            loss = loss.sum()
+        return loss
+
+
+def rmrf_mkdir(dir_name):
+    if os.path.exists(dir_name):
+        shutil.rmtree(dir_name)
+    os.mkdir(dir_name)
+
+def rm_file(path_file):
+    if os.path.exists(path_file):
+        os.remove(path_file)
+
+def colorize_mask(mask):
+    # mask: numpy array of the mask
+    new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
+    new_mask.putpalette(cfg.VIS.PALETTE_LABEL_COLORS)
+
+    return new_mask
+
+#============================
+
+
+def _fast_hist(label_true, label_pred, n_class):
+    mask = (label_true >= 0) & (label_true < n_class)
+    hist = np.bincount(
+        n_class * label_true[mask].astype(int) +
+        label_pred[mask], minlength=n_class**2).reshape(n_class, n_class)
+    return hist
+
+
+def scores(label_trues, label_preds, n_class):
+    """Returns accuracy score evaluation result.
+      - overall accuracy
+      - mean accuracy
+      - mean IU
+      - fwavacc
+    """
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    iu = np.diag(hist) / (hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist))
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    cls_iu = dict(zip(range(n_class), iu))
+
+    return {'Overall Acc: \t': acc,
+            'Mean Acc : \t': acc_cls,
+            'FreqW Acc : \t': fwavacc,
+            'Mean IoU : \t': mean_iu,}, cls_iu
+
+
+def compute_flops(net):
+    input_shape = (cfg.TRAIN.BATCH_SIZE, 3, cfg.TRAIN.IMG_SIZE[0], cfg.TRAIN.IMG_SIZE[1])
+    inputs = torch.randn(*input_shape).cuda()
+    flops, params = profile(net, inputs=(inputs,))
+    flops, params = clever_format([flops, params], "%.2f")
+    return flops, params
+    
+def compute_model_size(net):
+    num_params = sum(p.numel() for p in net.parameters())
+    model_size = num_params * 4 / (1024 ** 2)
+    return model_size
+
+def get_world_size():
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
+
+def reduce_loss_dict(loss_dict):
+    """
+    Reduce the loss dictionary from all processes so that process with rank
+    0 has the averaged results. Returns a dict with the same fields as
+    loss_dict, after reduction.
+    """
+    world_size = get_world_size()
+    if world_size < 2:
+        return loss_dict
+    with torch.no_grad():
+        loss_names = []
+        all_losses = []
+        for k in sorted(loss_dict.keys()):
+            loss_names.append(k)
+            all_losses.append(loss_dict[k])
+        all_losses = torch.stack(all_losses, dim=0)
+        dist.reduce(all_losses, dst=0)
+        if dist.get_rank() == 0:
+            # only main process gets accumulated, so only divide by
+            # world_size in this case
+            all_losses /= world_size
+        reduced_losses = {k: v for k, v in zip(loss_names, all_losses)}
+    return reduced_losses
+
+
+
