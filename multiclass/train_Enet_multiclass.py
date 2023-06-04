989995501ed1cfbe671e2d2d3842f6e152b69ebc
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-04 12:31:42 +0200

    Update train_Enet_multiclass.py

--- multiclass/train_Enet_multiclass.py
+++ multiclass/train_Enet_multiclass.py
@@ -56,7 +56,7 @@ def main():
         net=net.cuda()
 
     net.train()
-    criterion = criterion = torch.nn.CrossEntropyLoss().cuda() #loss multiclassification
+    criterion = torch.nn.CrossEntropyLoss().cuda() #loss multiclassification
 
     optimizer = optim.Adam(net.parameters(), lr=cfg.TRAIN.LR, weight_decay=cfg.TRAIN.WEIGHT_DECAY)
     scheduler = StepLR(optimizer, step_size=cfg.TRAIN.NUM_EPOCH_LR_DECAY, gamma=cfg.TRAIN.LR_DECAY)
