b551742879476383fad6de6c594a03fb8cfa8265
Gabriele Sanmartino <130171949+GabrieleSanmartino@users.noreply.github.com>
2023-06-06 10:35:46 +0000

    Custom

--- multiclass/train_BiSeNet_multiclass2.py
+++ multiclass/train_BiSeNet_multiclass2.py
@@ -48,9 +48,8 @@ def main():
 
     net.train()
     #criterion = torch.nn.CrossEntropyLoss()
-    #criterion = CustomLoss()
-    focal_loss_criterion = FocalLossV2(alpha=0.25, gamma=2, reduction='mean')
-    criterion = focal_loss_criterion()
+    criterion = CustomLoss()
+    #criterion = FocalLossV2(alpha=0.25, gamma=2, reduction='mean')
     criterion.cuda()
 
     optimizer = optim.Adam(net.parameters(), lr=cfg.TRAIN.LR, weight_decay=cfg.TRAIN.WEIGHT_DECAY)
