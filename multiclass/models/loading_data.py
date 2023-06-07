0331c302f585d87d6f65fa6e2b03a0936939ecf3
Gabriele Sanmartino <130171949+GabrieleSanmartino@users.noreply.github.com>
2023-06-07 13:29:06 +0000

    jit

--- multiclass/models/loading_data.py
+++ multiclass/models/loading_data.py
@@ -10,7 +10,7 @@ def loading_data():
         own_transforms.Scale(int(cfg.TRAIN.IMG_SIZE[0] / 0.875)),
         own_transforms.RandomCrop(cfg.TRAIN.IMG_SIZE),
         own_transforms.RandomHorizontallyFlip(),
-        own_transforms.ColorJitter()
+        own_transforms.ColorJitter(cfg.TRAIN.IMG_SIZE)
 
     ])
     val_simul_transform = own_transforms.Compose([
