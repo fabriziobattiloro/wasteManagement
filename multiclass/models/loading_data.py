cf278705f0a29c41bcf0e23235986c0587ead7c6
Gabriele Sanmartino <130171949+GabrieleSanmartino@users.noreply.github.com>
2023-06-07 13:06:50 +0000

    newMask

--- multiclass/models/loading_data.py
+++ multiclass/models/loading_data.py
@@ -4,16 +4,27 @@ import models.transforms as own_transforms
 from .resortit import resortit
 from .config import cfg
 
+#new transformation
+from torchvision.transforms import RandomRotation
+from torchvision.transforms import ColorJitter
+from torchvision.transforms import Resize
+
 def loading_data():
     mean_std = cfg.DATA.MEAN_STD
     train_simul_transform = own_transforms.Compose([
         own_transforms.Scale(int(cfg.TRAIN.IMG_SIZE[0] / 0.875)),
         own_transforms.RandomCrop(cfg.TRAIN.IMG_SIZE),
         own_transforms.RandomHorizontallyFlip()
+        #new
+        RandomRotation(30)
+        ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)
+
     ])
     val_simul_transform = own_transforms.Compose([
         own_transforms.Scale(int(cfg.TRAIN.IMG_SIZE[0] / 0.875)),
         own_transforms.CenterCrop(cfg.TRAIN.IMG_SIZE)
+        #new
+        Resize((256, 256))
     ])
     img_transform = standard_transforms.Compose([
         standard_transforms.ToTensor(),
