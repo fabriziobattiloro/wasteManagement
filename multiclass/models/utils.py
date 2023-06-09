95db7bb04fda073e0fc6673533e87431258d0d49
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 09:49:09 +0200

    Update utils.py

--- multiclass/models/utils.py
+++ multiclass/models/utils.py
@@ -237,7 +237,6 @@ class AutomaticWeightedLoss(nn.Module):
         super(AutomaticWeightedLoss, self).__init__()
         params = torch.ones(num, requires_grad=True)
         self.params = torch.nn.Parameter(params)
-
     def forward(self, *x):
         loss_sum = 0
         for i, loss in enumerate(x):
@@ -248,4 +247,44 @@ if __name__ == '__main__':
     awl = AutomaticWeightedLoss(2)
     print(awl.parameters())
 
+##load model xception39
+
+def load_model(model, model_file, is_restore=False):
+    t_start = time.time()
+    if isinstance(model_file, str):
+        state_dict = torch.load(model_file, map_location=torch.device('cpu'))
+        if 'model' in state_dict.keys():
+            state_dict = state_dict['model']
+    else:
+        state_dict = model_file
+    t_ioend = time.time()
+
+    if is_restore:
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = 'module.' + k
+            new_state_dict[name] = v
+        state_dict = new_state_dict
+
+    model.load_state_dict(state_dict, strict=False)
+    ckpt_keys = set(state_dict.keys())
+    own_keys = set(model.state_dict().keys())
+    missing_keys = own_keys - ckpt_keys
+    unexpected_keys = ckpt_keys - own_keys
+
+    if len(missing_keys) > 0:
+        logger.warning('Missing key(s) in state_dict: {}'.format(
+            ', '.join('{}'.format(k) for k in missing_keys)))
+
+    if len(unexpected_keys) > 0:
+        logger.warning('Unexpected key(s) in state_dict: {}'.format(
+            ', '.join('{}'.format(k) for k in unexpected_keys)))
+
+    del state_dict
+    t_end = time.time()
+    logger.info(
+        "Load model, Time usage:\n\tIO: {}, initialize parameters: {}".format(
+            t_ioend - t_start, t_end - t_ioend))
+
+    return model
 
