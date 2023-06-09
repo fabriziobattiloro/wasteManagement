1fdcfa5d977cd07c7c4383a6a74bf104a2201d11
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 16:06:10 +0200

    Update train_BiSeNet_multiclass.py

--- multiclass/train_BiSeNet_multiclass.py
+++ multiclass/train_BiSeNet_multiclass.py
@@ -143,7 +143,7 @@ def validate(val_loader, net, criterion, optimizer, epoch, restore, class_counts
     # Print the pixel counts for each class
     for c in range(cfg.DATA.NUM_CLASSES):
         if class_counts[c]==0:
-            class_count[c]=1
+            class_counts[c]=1
         print(f"pixels {c}: {class_counts[c]} pixels")
   
     print("|||||||||||")
