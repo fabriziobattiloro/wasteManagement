22917a5c67a14109a5f7fab693b573c20af737d5
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 16:23:30 +0200

    Update train_BiSeNet_multiclass.py

--- multiclass/train_BiSeNet_multiclass.py
+++ multiclass/train_BiSeNet_multiclass.py
@@ -48,8 +48,8 @@ def main():
 
     net.train()
     class_counts = [1] * cfg.DATA.NUM_CLASSES
-    #criterion = CB_Loss(1, class_counts[0]/class_counts[1], class_counts[0]/class_counts[2], class_counts[0]/class_counts[3], class_counts[0]/class_counts[4])
-    criterion=CB_Loss(1,1,1,1,1)
+    criterion = CB_Loss(loss_type="binary_cross_entropy",1, class_counts[0]/class_counts[1], class_counts[0]/class_counts[2], class_counts[0]/class_counts[3], class_counts[0]/class_counts[4])
+    #criterion=CB_Loss(loss_type="binary_cross_entropy",1,1,1,1,1)
     criterion.cuda()
 
    
