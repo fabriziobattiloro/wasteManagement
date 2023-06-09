d5913de4c383ecab3cc9fabe209882c40ba926bf
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 17:34:29 +0200

    Update loss.py

--- multiclass/models/loss.py
+++ multiclass/models/loss.py
@@ -105,39 +105,38 @@ def focal_loss(labels, logits, alpha, gamma):
 
 
 
-def CB_loss(labels, logits, samples_per_cls, no_of_classes, loss_type, beta, gamma):
+def CB_loss(nn.Module):
     """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
     Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
     where Loss is one of the standard losses used for Neural Networks.
-    Args:
-      labels: A int tensor of size [batch].
-      logits: A float tensor of size [batch, no_of_classes].
-      samples_per_cls: A python list of size [no_of_classes].
-      no_of_classes: total number of classes. int
-      loss_type: string. One of "sigmoid", "focal", "softmax".
-      beta: float. Hyperparameter for Class balanced loss.
-      gamma: float. Hyperparameter for Focal loss.
-    Returns:
-      cb_loss: A float tensor representing class balanced loss
     """
-    effective_num = 1.0 - np.power(beta, samples_per_cls)
-    weights = (1.0 - beta) / np.array(effective_num)
-    weights = weights / np.sum(weights) * no_of_classes
+    def __init__(self, samples_per_cls, no_of_classes=cfg.DATA_NUM_CLASSES, loss_type="focal_loss"=, beta=0.9999, gamma=2.0):
+        super(CB_loss, self).__init__()
+        self.samples_per_cls = samples_per_cls
+        self.no_of_classes = no_of_classes
+        self.loss_type = loss_type
+        self.beta = beta
+        self.gamma = gamma
+        
+    def forward(self, labels, logits):    
+        effective_num = 1.0 - np.power(beta, samples_per_cls)
+        weights = (1.0 - beta) / np.array(effective_num)
+        weights = weights / np.sum(weights) * no_of_classes
 
-    labels_one_hot = F.one_hot(labels, no_of_classes).float()
+        labels_one_hot = F.one_hot(labels, no_of_classes).float()
 
-    weights = torch.tensor(weights).float()
-    weights = weights.unsqueeze(0)
-    weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
-    weights = weights.sum(1)
-    weights = weights.unsqueeze(1)
-    weights = weights.repeat(1,no_of_classes)
+        weights = torch.tensor(weights).float()
+        weights = weights.unsqueeze(0)
+        weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
+        weights = weights.sum(1)
+        weights = weights.unsqueeze(1)
+        weights = weights.repeat(1,no_of_classes)
 
-    if loss_type == "focal":
-        cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
-    elif loss_type == "sigmoid":
-        cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
-    elif loss_type == "softmax":
-        pred = logits.softmax(dim = 1)
-        cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
-    return cb_loss
+        if loss_type == "focal":
+            cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
+        elif loss_type == "sigmoid":
+            cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
+        elif loss_type == "softmax":
+            pred = logits.softmax(dim = 1)
+            cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
+        return cb_loss
