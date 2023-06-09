2fc38546e3ff98d51fdd0ec2977530b17378fd61
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 17:20:50 +0200

    Update loss.py

--- multiclass/models/loss.py
+++ multiclass/models/loss.py
@@ -73,125 +73,71 @@ import torch
 import torch.nn.functional as F
 
 
-def focal_loss(logits, labels, alpha=None, gamma=2):
+def focal_loss(labels, logits, alpha, gamma):
     """Compute the focal loss between `logits` and the ground truth `labels`.
     Focal loss = -alpha_t * (1-pt)^gamma * log(pt)
     where pt is the probability of being classified to the true class.
     pt = p (if true class), otherwise pt = 1 - p. p = sigmoid(logit).
     Args:
-      logits: A float tensor of size [batch, num_classes].
       labels: A float tensor of size [batch, num_classes].
+      logits: A float tensor of size [batch, num_classes].
       alpha: A float tensor of size [batch_size]
         specifying per-example weight for balanced cross entropy.
       gamma: A float scalar modulating loss from hard and easy examples.
     Returns:
       focal_loss: A float32 scalar representing normalized total loss.
     """
-    bc_loss = F.binary_cross_entropy_with_logits(input=logits, target=labels, reduction="none")
+    BCLoss = F.binary_cross_entropy_with_logits(input = logits, target = labels,reduction = "none")
 
     if gamma == 0.0:
         modulator = 1.0
     else:
-        modulator = torch.exp(-gamma * labels * logits - gamma * torch.log(1 + torch.exp(-1.0 * logits)))
+        modulator = torch.exp(-gamma * labels * logits - gamma * torch.log(1 +
+            torch.exp(-1.0 * logits)))
 
-    loss = modulator * bc_loss
+    loss = modulator * BCLoss
 
-    if alpha is not None:
-        weighted_loss = alpha * loss
-        focal_loss = torch.sum(weighted_loss)
-    else:
-        focal_loss = torch.sum(loss)
+    weighted_loss = alpha * loss
+    focal_loss = torch.sum(weighted_loss)
 
     focal_loss /= torch.sum(labels)
     return focal_loss
 
 
-class Loss(torch.nn.Module):
-    def __init__(
-        self,
-        loss_type: str = "cross_entropy",
-        beta: float = 0.999,
-        fl_gamma=2,
-        samples_per_class=None,
-        class_balanced=False,
-    ):
-        """
-        Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
-        Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
-        where Loss is one of the standard losses used for Neural Networks.
 
-        reference: https://openaccess.thecvf.com/content_CVPR_2019/papers/Cui_Class-Balanced_Loss_Based_on_Effective_Number_of_Samples_CVPR_2019_paper.pdf
+def CB_loss(labels, logits, samples_per_cls, no_of_classes, loss_type, beta, gamma):
+    """Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
+    Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
+    where Loss is one of the standard losses used for Neural Networks.
+    Args:
+      labels: A int tensor of size [batch].
+      logits: A float tensor of size [batch, no_of_classes].
+      samples_per_cls: A python list of size [no_of_classes].
+      no_of_classes: total number of classes. int
+      loss_type: string. One of "sigmoid", "focal", "softmax".
+      beta: float. Hyperparameter for Class balanced loss.
+      gamma: float. Hyperparameter for Focal loss.
+    Returns:
+      cb_loss: A float tensor representing class balanced loss
+    """
+    effective_num = 1.0 - np.power(beta, samples_per_cls)
+    weights = (1.0 - beta) / np.array(effective_num)
+    weights = weights / np.sum(weights) * no_of_classes
 
-        Args:
-            loss_type: string. One of "focal_loss", "cross_entropy",
-                "binary_cross_entropy", "softmax_binary_cross_entropy".
-            beta: float. Hyperparameter for Class balanced loss.
-            fl_gamma: float. Hyperparameter for Focal loss.
-            samples_per_class: A python list of size [num_classes].
-                Required if class_balance is True.
-            class_balanced: bool. Whether to use class balanced loss.
-        Returns:
-            Loss instance
-        """
-        super(Loss, self).__init__()
+    labels_one_hot = F.one_hot(labels, no_of_classes).float()
 
-        if class_balanced is True and samples_per_class is None:
-            raise ValueError("samples_per_class cannot be None when class_balanced is True")
-        #print(self.loss_type)    
-        #self.loss_type = loss_type
-        #print(self.loss_type)
-        self.beta = beta
-        self.fl_gamma = fl_gamma
-        self.samples_per_class = samples_per_class
-        self.class_balanced = class_balanced
+    weights = torch.tensor(weights).float()
+    weights = weights.unsqueeze(0)
+    weights = weights.repeat(labels_one_hot.shape[0],1) * labels_one_hot
+    weights = weights.sum(1)
+    weights = weights.unsqueeze(1)
+    weights = weights.repeat(1,no_of_classes)
 
-    def forward(
-        self,
-        logits: torch.tensor,
-        labels: torch.tensor,
-    ):
-        """
-        Compute the Class Balanced Loss between `logits` and the ground truth `labels`.
-        Class Balanced Loss: ((1-beta)/(1-beta^n))*Loss(labels, logits)
-        where Loss is one of the standard losses used for Neural Networks.
-
-        Args:
-            logits: A float tensor of size [batch, num_classes].
-            labels: An int tensor of size [batch].
-        Returns:
-            cb_loss: A float tensor representing class balanced loss
-        """
-
-        batch_size = logits.size(0)
-        num_classes = logits.size(1)
-        labels_one_hot = F.one_hot(labels, num_classes).float()
-
-        if self.class_balanced:
-            effective_num = 1.0 - np.power(self.beta, self.samples_per_class)
-            weights = (1.0 - self.beta) / np.array(effective_num)
-            weights = weights / np.sum(weights) * num_classes
-            weights = torch.tensor(weights, device=logits.device).float()
-
-            if self.loss_type != "cross_entropy":
-                weights = weights.unsqueeze(0)
-                weights = weights.repeat(batch_size, 1) * labels_one_hot
-                weights = weights.sum(1)
-                weights = weights.unsqueeze(1)
-                weights = weights.repeat(1, num_classes)
-        else:
-            weights = None
-
-        if self.loss_type == "focal_loss":
-            print("SIUUUUM")
-            cb_loss = focal_loss(logits, labels_one_hot, alpha=weights, gamma=self.fl_gamma)
-        elif self.loss_type == "cross_entropy":
-            cb_loss = F.cross_entropy(input=logits, target=labels_one_hot, weight=weights)
-        elif self.loss_type == "binary_cross_entropy":
-            cb_loss = F.binary_cross_entropy_with_logits(input=logits, target=labels_one_hot, weight=weights)
-        elif self.loss_type == "softmax_binary_cross_entropy":
-            pred = logits.softmax(dim=1)
-            cb_loss = F.binary_cross_entropy(input=pred, target=labels_one_hot, weight=weights)
-        else:
-            print("siuuuum")
-            cb_loss=None
-        return cb_loss
+    if loss_type == "focal":
+        cb_loss = focal_loss(labels_one_hot, logits, weights, gamma)
+    elif loss_type == "sigmoid":
+        cb_loss = F.binary_cross_entropy_with_logits(input = logits,target = labels_one_hot, weights = weights)
+    elif loss_type == "softmax":
+        pred = logits.softmax(dim = 1)
+        cb_loss = F.binary_cross_entropy(input = pred, target = labels_one_hot, weight = weights)
+    return cb_loss
