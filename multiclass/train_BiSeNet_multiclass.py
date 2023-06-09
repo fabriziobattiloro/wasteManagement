bd11c86be7f2f3b87ccf2ed938cc1a0fbe5ae759
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-09 16:01:56 +0200

    Update train_BiSeNet_multiclass.py

--- multiclass/train_BiSeNet_multiclass.py
+++ multiclass/train_BiSeNet_multiclass.py
@@ -47,7 +47,7 @@ def main():
         net=net.cuda()
 
     net.train()
-    class_counts = [0] * cfg.DATA.NUM_CLASSES
+    class_counts = [1] * cfg.DATA.NUM_CLASSES
     criterion = CB_Loss(1, class_counts[0]/class_counts[1], class_counts[0]/class_counts[2], class_counts[0]/class_counts[3], class_counts[0]/class_counts[4])
     criterion.cuda()
 
