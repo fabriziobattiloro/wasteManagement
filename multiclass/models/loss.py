b4cfd4fc5f0244186de422c9bcbcfab6482413cb
LorenzoMossotto <129867778+LorenzoMossotto@users.noreply.github.com>
2023-06-10 17:58:45 +0200

    Update loss.py

--- multiclass/models/loss.py
+++ multiclass/models/loss.py
@@ -182,7 +182,8 @@ class CB_loss(torch.nn.Module):
                 weights = weights.repeat(batch_size, 1)
                 print("weights dimensions:", weights.size())
                 weights = weights.unsqueeze(2)
-                weights = weights.expand(-1, -1, num_classes)
+                weights = weights.unsqueeze(3)
+                weights = weights.expand(-1, labels_one_hot.size(1), labels_one_hot.size(2), labels_one_hot.size(3))
                 print("weights dimensions:", weights.size())
                 weights = weights * labels_one_hot
                 weights = weights.sum(1)
