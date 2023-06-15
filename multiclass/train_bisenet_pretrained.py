062702a4f67ce52a84d282ac8e02f1f73b75805e
fabriziobattiloro <129081348+fabriziobattiloro@users.noreply.github.com>
2023-06-15 23:58:53 +0000

    .

--- multiclass/train_bisenet_pretrained.py
+++ multiclass/train_bisenet_pretrained.py
@@ -4,21 +4,20 @@ import random
 import torch
 from torch import optim
 from torch.autograd import Variable
+import torch.nn.functional as F
 from torch.nn import NLLLoss2d
 from torch.optim.lr_scheduler import StepLR
 from torchvision.utils import save_image
 import torchvision.transforms as standard_transforms
 import torchvision.utils as vutils
 from tensorboardX import SummaryWriter
-import torch.nn.functional as F
 
-from models.pretrained_model_bisenet import BiSeNet
+from models.model_BiSeNet2 import BiSeNet
 from models.config import cfg, __C
 from models.loading_data import loading_data
 from models.utils import *
 from models.timer import Timer
 from models.loss import MixSoftmaxCrossEntropyLoss
-from multiclass.models.pretrained_model_bisenet import BiSeNet
 import pdb
 
 exp_name = cfg.TRAIN.EXP_NAME
@@ -27,6 +26,7 @@ writer = SummaryWriter(cfg.TRAIN.EXP_PATH+ '/' + exp_name)
 
 pil_to_tensor = standard_transforms.ToTensor()
 train_loader, val_loader, restore_transform = loading_data()
+
 def main():
 
     cfg_file = open('/kaggle/working/project-code1/multiclass/models/config.py',"r")  
@@ -37,8 +37,6 @@ def main():
     if len(cfg.TRAIN.GPU_ID)==1:
         torch.cuda.set_device(cfg.TRAIN.GPU_ID[0])
     torch.backends.cudnn.benchmark = True
-
-    train_rotation_model()
 
     net = []  
     net = BiSeNet(cfg.DATA.NUM_CLASSES, 'resnet50') 
@@ -52,12 +50,10 @@ def main():
     criterion = torch.nn.CrossEntropyLoss()
     criterion.cuda()
 
-   
     optimizer = optim.Adam(net.parameters(), lr=cfg.TRAIN.LR, weight_decay=cfg.TRAIN.WEIGHT_DECAY)
     scheduler = StepLR(optimizer, step_size=cfg.TRAIN.NUM_EPOCH_LR_DECAY, gamma=cfg.TRAIN.LR_DECAY)
     _t = {'train time' : Timer(),'val time' : Timer()} 
     validate(val_loader, net, criterion, optimizer, -1, restore_transform)
-    
     for epoch in range(cfg.TRAIN.MAX_EPOCH):
         _t['train time'].tic()
         train(train_loader, net, criterion, optimizer, epoch)
@@ -76,16 +72,11 @@ def train(train_loader, net, criterion, optimizer, epoch):
         labels = Variable(labels).cuda()
 
         outputs = net(inputs)
-        out1, out2, out3= outputs
         # Resize the labels tensor to match the output tensor dimensions
 
-        loss1 = criterion(out1, labels)
-        loss2 = criterion(out2, labels)
-        loss3 = criterion(out3, labels)
-
-        losses = loss1 + loss2 + loss3
+        loss = criterion(outputs[0], labels)
         optimizer.zero_grad()
-        losses.backward()
+        loss.backward()
         optimizer.step()
 
 
@@ -110,12 +101,10 @@ def validate(val_loader, net, criterion, optimizer, epoch, restore):
         inputs = Variable(inputs, volatile=True).cuda()
         labels = Variable(labels, volatile=True).cuda()
         outputs = net(inputs)
-        out1, out2, out3 = outputs
-
-        #out1 = F.softmax(out1, dim=1)  # Apply softmax activation function along the channel dimension
+        outputs = outputs[0]
         
         # For each pixel, determine the class with highest probability
-        max_value, predicted = torch.max(out1.data, 1)  
+        max_value, predicted = torch.max(outputs.data, 1)  
         
         input_batches.append(inputs)
         output_batches.append(predicted)
@@ -138,11 +127,11 @@ def validate(val_loader, net, criterion, optimizer, epoch, restore):
     print(f"Class tot: {mean_tot / len(val_loader):.4f}")
   
   
+    model_size = compute_model_size(net)
+    print('Model size %.4f' % (model_size))
     # Calculate average IoU score over all classes
     #mean_classes[5] =float (iou_ / cfg.DATA.NUM_CLASSES)
     #print(f"Mean IoU: {mean_iou:.4f}")
-    model_size = compute_model_size(net)
-    print('Model size %.4f' % (model_size))
 
     net.train()
     criterion.cuda()
@@ -150,7 +139,6 @@ def validate(val_loader, net, criterion, optimizer, epoch, restore):
 
 if __name__ == '__main__':
     main()
-
 
 
 
